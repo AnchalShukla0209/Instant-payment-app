63e6668c0852d9f5a610712eae619ea44ad05f75
Ayush Awasthi <ayushawasthi731@gmail.com>
2026-08-18 16:29:56 +0530

    fix: isolate website development API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
     //apiBaseUrl: 'https://testapi-ip.thedemo.co.in/api'
     //apiBaseUrl: 'https://api.instantpayment.co.in/api'
-    apiBaseUrl:'https://localhost:7003/api'
+    apiBaseUrl:'http://127.0.0.1:7004/api'
 };
